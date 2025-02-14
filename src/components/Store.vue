803e5cff7caa6571c8d380dd0d9943e58170e2bf
Ravindu <85988392+Ravindu26881@users.noreply.github.com>
2025-02-14 10:20:37 +0530

    update

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,102 +1,70 @@
 <template>
-<!--  <nav-bar :isMobile="true"/>-->
   <div class="store__wrapper">
     <div class="company-logo" v-if="!isMobile">
       <div>Foot</div><div style="color: #53220080;">wear</div><div>Store</div>
     </div>
 
     <div class="item-list__wrapper">
-          <div v-for="item in itemList" :key="item.text" class="item">
-            <img :src="item.img" alt="item" class="item__img"/>
-            <div class="item-list__Text">
-              <div class="item__text">{{item.text}}</div>
-              <div class="item__price">{{item.price}}</div>
-            </div>
+      <div class="filter-wrapper">
+        <input
+            type="text"
+            class="form-control filter-search-input"
+            placeholder="Search for items"
+            v-model="searchQuery"
+            aria-label="Search for items"
+            aria-describedby="button-addon2"
+        />
+      </div>
 
+      <div class="container">
+        <div class="row">
+          <div v-for="item in filteredItems" :key="item.text" class="col-md-4 col-6 mb-4">
+            <div class="card item shadow-sm p-3 text-center">
+              <img :src="item.img" alt="item" class="card-img-top item__img"/>
+              <div class="card-body">
+                <h5 class="card-title">{{ item.text }}</h5>
+                <p class="card-text text-muted">{{ item.price }}</p>
+              </div>
+            </div>
           </div>
+          <div v-if="filteredItems.length === 0" class="col-12 text-center mt-3">
+            <p>No items found.</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
-// import NavBar from "@/components/navigators/NavBar.vue";
-
 export default {
   name: 'ItemStore',
-  components: {
-    // NavBar
-  },
   data() {
     return {
+      searchQuery: "",
       itemList: [
-        {
-          img: require('@/assets/img/shoe1.png'),
-          text: 'Runners',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe2.png'),
-          text: 'Walkers',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe1.png'),
-          text: 'Joggers',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe2.png'),
-          text: 'Sprinters',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe1.png'),
-          text: 'Casual',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe2.png'),
-          text: 'Sneakers',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe1.png'),
-          text: 'Runners',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe2.png'),
-          text: 'Walkers',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe1.png'),
-          text: 'Joggers',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe2.png'),
-          text: 'Sprinters',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe1.png'),
-          text: 'Casual',
-          price: 'Coming Soon'
-        },
-        {
-          img: require('@/assets/img/shoe2.png'),
-          text: 'Sneakers',
-          price: 'Coming Soon'
-        }
+        { img: require('@/assets/img/shoe1.png'), text: 'Runners', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe2.png'), text: 'Walkers', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe1.png'), text: 'Joggers', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe2.png'), text: 'Sprinters', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe1.png'), text: 'Casual', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe2.png'), text: 'Sneakers', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe1.png'), text: 'Runners', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe2.png'), text: 'Walkers', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe1.png'), text: 'Joggers', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe2.png'), text: 'Sprinters', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe1.png'), text: 'Casual', price: 'Coming Soon' },
+        { img: require('@/assets/img/shoe2.png'), text: 'Sneakers', price: 'Coming Soon' }
       ]
     };
   },
-  methods: {
-  },
-  mounted() {
-  },
+  computed: {
+    filteredItems() {
+      if (!this.searchQuery) return this.itemList; // Show all items if search is empty
+      const query = this.searchQuery.toLowerCase();
+      return this.itemList.filter(item => item.text.toLowerCase().includes(query));
+    }
+  }
 };
 </script>
 
@@ -111,27 +79,24 @@ export default {
 .company-logo {
   display: flex;
 }
-.gc-navbar__wrapper-mobile {
-  padding: 20px 20px 0 0;
-  color: black;
-}
-
 .item__img {
   width: 140px;
 }
 .item {
-  display: flex;
-  align-items: center;
+  text-align: center;
 }
 .item-list__wrapper {
-  display: flex;
   padding: 10px;
-  flex-wrap: wrap;
   background: white;
   border-radius: 21px;
   box-shadow: 0px 0px 20px 0px rgb(0 0 0 / 15%);
   margin-top: 26px;
-  flex-direction: column;
-  align-content: flex-start;
+}
+.filter-search-input {
+  width: 100%;
+  max-width: 300px;
+  border-radius: 21px;
+  padding: 10px;
+  margin-bottom: 15px;
 }
 </style>
